718eec13ae6a33761ef37040a1ff0611e4d1e1f3
Junaid Khan <dianujkotov15@gmail.com>
2021-07-10 18:36:45 +0900

    first commit

---
+++ cnn.py
@@ -0,0 +1,71 @@
+from torch import nn
+from torchsummary import summary
+
+
+class CNNNetwork(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        # 4 conv blocks / flatten / linear / softmax
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=96,
+                kernel_size=11,
+                stride=4,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=96,
+                out_channels=256,
+                kernel_size=5,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=256,
+                out_channels=384,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=384,
+                out_channels=256,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.flatten = nn.Flatten()
+        self.batch= nn.BatchNorm1d(1024)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, input_data):
+        x = self.conv1(input_data)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.flatten(x)
+        batcher= self.batch(x)
+        predictions = self.softmax(batcher)
+        return predictions
+
+
+if __name__ == "__main__":
+    cnn = CNNNetwork()
+    summary(cnn.cuda(), (1, 64, 44))
